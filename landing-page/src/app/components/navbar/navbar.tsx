018872bd94681033af52043d2d111b436a05bdf6
femy-dmng <femydomingo@proton.me>
2025-05-27 10:08:10 +0200

    update: navbar content

--- landing-page/src/app/components/navbar/navbar.tsx
+++ landing-page/src/app/components/navbar/navbar.tsx
@@ -1,24 +1,18 @@
 "use client";
 
+import Image from "next/image";
+import logo from "assests/logoTop.png";
 import styles from "@/app/components/navbar/navbar.module.scss";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
-
   return (
     <nav className={styles.navbar}>
-      <div>
-        <p className={styles.titleNavbar}>Shared Notes</p>
-        <p className={styles.download}></p>
+      <div className={styles.textNavbar}>
+        <Image src={logo} alt="Logo" width={35} height={35} />
+        <h3 className={styles.titleNavbar}>Shared Notes</h3>
+        <p className={styles.download}>Télécharger</p>
       </div>
     </nav>
   );
