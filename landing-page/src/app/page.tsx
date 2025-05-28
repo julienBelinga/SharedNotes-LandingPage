2cc9367570e66f90d4c32c183b61dea01561edb3
femy-dmng <femydomingo@proton.me>
2025-05-28 16:00:15 +0200

    feature: section introduction

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Herobanner from "./section/hero-banner/hero-banner";
+import Introduction from "./section/introduction/introduction";
 
 export default function Home() {
   return (
     <>
       <Herobanner />
+      <Introduction />
     </>
   );
 }
