3d6223995660a50a26c537d36516796b1c3991a3
femy-dmng <femydomingo@proton.me>
2025-05-30 17:16:27 +0200

    feature: multidevice files

---
+++ landing-page/src/app/section/mutlidevice/multidevice.tsx
@@ -0,0 +1,9 @@
+import styles from "./multidevice.module.scss";
+
+export default function Mutlidevice() {
+  return (
+    <section className={styles.multidevice}>
+      <h3 className={styles.title}>Sur Android et iOS.</h3>
+    </section>
+  );
+}
